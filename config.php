a2bdd52171ebdd1a0ea09985343adbec5d844d7f
Shamil <a_shoma@mail.ru>
2018-05-30 23:15:17 +0300

    meta data change

--- config.php
+++ config.php
@@ -5,6 +5,6 @@ define('SITE_CITY', 'МО. г Балашиха');
 define('SITE_ADDRESS', 'ул Советская, д 10а');
 define('ADMIN_EMAIL', 'europasia@list.ru');
 define('SITE_PHONE', '+7 901 753-13-42');
-define('SITE_NAME', 'Студия дизайна Европа Азия');
+define('SITE_NAME', 'Студия дизайна Шторы и Декор');
 
 ?>
--- view/pages/articles.php
+++
@@ -1,2 +0,0 @@
-
-<h2 class="page-title">Статьи</h2>
