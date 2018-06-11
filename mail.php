e6f8a504b5cc1df57526d9bbfb40f5b51f399671
Shamil <a_shoma@mail.ru>
2018-06-11 20:01:01 +0300

    первия версия лендинга

--- mail.php
+++ mail.php
@@ -6,18 +6,16 @@ function clear_data($data) {
   return $data;
 }
 
-
 // данные администратора
 $recepient = "zanaves@bk.ru";
-$recepient2 = "alisultanovshamil1992@gmail.com";
+$recepient2 = "Shtori-i-dekor@mail.ru";
+$recepient3 = "alisultanovshamil1992@gmail.com";
 $sitename = "shtori-i-dekor.ru";
 
 // Принимаемые данные
 $name = clear_data($_POST["order-name"]);
+$email = clear_data($_POST["order-email"]);
 $phone = clear_data($_POST["order-phone"]);
-
-//$name = $_POST["order-name"];
-//$phone = $_POST["order-phone"];
 
 // создаем сообщение
 $message = "
@@ -25,6 +23,10 @@ $message = "
 <tr>
 <td style='padding: 10px;'>Имя:</td>
 <td style='padding: 10px;'>$name</td>
+</tr>
+<tr>
+<td style='padding: 10px;'>Email:</td>
+<td style='padding: 10px;'>$email</td>
 </tr>
 <tr>
 <td style='padding: 10px;'>Телефон:</td>
@@ -37,7 +39,7 @@ $pagetitle = "Новая заявка с сайта \"$sitename\"";
 
 // отправляем
 mail($recepient, $pagetitle, $message, "Content-type: text/html; charset=\"utf-8\"\n From: $recepient");
-mail($recepient2, $pagetitle, $message, "Content-type: text/html; charset=\"utf-8\"\n From: $recepient");
-
+mail($recepient2, $pagetitle, $message, "Content-type: text/html; charset=\"utf-8\"\n From: $recepient2");
+mail($recepient3, $pagetitle, $message, "Content-type: text/html; charset=\"utf-8\"\n From: $recepient3");
 
 ?>
--- static/js/main.js
+++ static/js/main.js
@@ -50,7 +50,6 @@
         $(this).parent().children('.top-menu__list').stop().slideToggle();
     });
 
-
     // Отправка заявки
     $(".js-order-form").submit(function () {
         $.ajax({
@@ -73,5 +72,199 @@
         return false;
     });
 
+    // $("#lightgalery").lightGallery({});
+
+    // gallery start
+    $('.js-gallery-run').on('click', function (event) {
+        event.preventDefault();
+
+        var result = null;
+        var currentGallery = $(this).attr('data-href');
+
+        switch (currentGallery) {
+            case 'gallery1':
+                result = gallery1;
+                break;
+            case 'gallery2':
+                result = gallery2;
+                break;
+            case 'gallery3':
+                result = gallery3;
+                break;
+            case 'gallery4':
+                result = gallery4;
+                break;
+            case 'gallery5':
+                result = gallery5;
+                break;
+            case 'gallery6':
+                result = gallery6;
+                break;
+            case 'gallery7':
+                result = gallery7;
+                break;
+            case 'gallery8':
+                result = gallery8;
+                break;
+            case 'gallery9':
+                result = gallery9;
+                break;
+            case 'gallery10':
+                result = gallery10;
+                break;
+        }
+
+        $(this).lightGallery({
+            dynamic: true,
+            counter: true,
+            addClass: 'shamil-frontend',
+            width: '90%',
+            height: '90%',
+            dynamicEl: result
+        })
+    });
+
+    // gallery
+    var gallery1 = [
+        {
+            "src": './static/media/portfolio/27519_DSC_0332.JPG',
+            'thumb': './static/img/gallery-work/9.jpg'
+        },
+        {
+            'src': './static/media/portfolio/27519_DSC_0332.JPG',
+            'thumb': './static/img/gallery-work/9.jpg'
+        },
+        {
+            'src': './static/media/portfolio/27519_DSC_0332.JPG',
+            'thumb': './static/img/gallery-work/9.jpg'
+        }
+    ];
+    var gallery2 = [
+        {
+            "src": './static/media/portfolio/27519_DSC_0332.JPG',
+            'thumb': './static/img/gallery-work/9.jpg'
+        },
+        {
+            'src': './static/media/portfolio/27519_DSC_0332.JPG',
+            'thumb': './static/img/gallery-work/9.jpg'
+        },
+        {
+            'src': './static/media/portfolio/27519_DSC_0332.JPG',
+            'thumb': './static/img/gallery-work/9.jpg'
+        }
+    ];
+    var gallery3 = [
+        {
+            "src": './static/media/portfolio/27519_DSC_0332.JPG',
+            'thumb': './static/img/gallery-work/9.jpg'
+        },
+        {
+            'src': './static/media/portfolio/27519_DSC_0332.JPG',
+            'thumb': './static/img/gallery-work/9.jpg'
+        },
+        {
+            'src': './static/media/portfolio/27519_DSC_0332.JPG',
+            'thumb': './static/img/gallery-work/9.jpg'
+        }
+    ];
+    var gallery4 = [
+        {
+            "src": './static/media/portfolio/27519_DSC_0332.JPG',
+            'thumb': './static/img/gallery-work/9.jpg'
+        },
+        {
+            'src': './static/media/portfolio/27519_DSC_0332.JPG',
+            'thumb': './static/img/gallery-work/9.jpg'
+        },
+        {
+            'src': './static/media/portfolio/27519_DSC_0332.JPG',
+            'thumb': './static/img/gallery-work/9.jpg'
+        }
+    ];
+    var gallery5 = [
+        {
+            "src": './static/media/portfolio/27519_DSC_0332.JPG',
+            'thumb': './static/img/gallery-work/9.jpg'
+        },
+        {
+            'src': './static/media/portfolio/27519_DSC_0332.JPG',
+            'thumb': './static/img/gallery-work/9.jpg'
+        },
+        {
+            'src': './static/media/portfolio/27519_DSC_0332.JPG',
+            'thumb': './static/img/gallery-work/9.jpg'
+        }
+    ];
+    var gallery6 = [
+        {
+            "src": './static/media/portfolio/27519_DSC_0332.JPG',
+            'thumb': './static/img/gallery-work/9.jpg'
+        },
+        {
+            'src': './static/media/portfolio/27519_DSC_0332.JPG',
+            'thumb': './static/img/gallery-work/9.jpg'
+        },
+        {
+            'src': './static/media/portfolio/27519_DSC_0332.JPG',
+            'thumb': './static/img/gallery-work/9.jpg'
+        }
+    ];
+    var gallery7 = [
+        {
+            "src": './static/media/portfolio/27519_DSC_0332.JPG',
+            'thumb': './static/img/gallery-work/9.jpg'
+        },
+        {
+            'src': './static/media/portfolio/27519_DSC_0332.JPG',
+            'thumb': './static/img/gallery-work/9.jpg'
+        },
+        {
+            'src': './static/media/portfolio/27519_DSC_0332.JPG',
+            'thumb': './static/img/gallery-work/9.jpg'
+        }
+    ];
+    var gallery8 = [
+        {
+            "src": './static/media/portfolio/27519_DSC_0332.JPG',
+            'thumb': './static/img/gallery-work/9.jpg'
+        },
+        {
+            'src': './static/media/portfolio/27519_DSC_0332.JPG',
+            'thumb': './static/img/gallery-work/9.jpg'
+        },
+        {
+            'src': './static/media/portfolio/27519_DSC_0332.JPG',
+            'thumb': './static/img/gallery-work/9.jpg'
+        }
+    ];
+    var gallery9 = [
+        {
+            "src": './static/media/portfolio/27519_DSC_0332.JPG',
+            'thumb': './static/img/gallery-work/9.jpg'
+        },
+        {
+            'src': './static/media/portfolio/27519_DSC_0332.JPG',
+            'thumb': './static/img/gallery-work/9.jpg'
+        },
+        {
+            'src': './static/media/portfolio/27519_DSC_0332.JPG',
+            'thumb': './static/img/gallery-work/9.jpg'
+        }
+    ];
+    var gallery10 = [
+        {
+            "src": './static/media/portfolio/27519_DSC_0332.JPG',
+            'thumb': './static/img/gallery-work/9.jpg'
+        },
+        {
+            'src': './static/media/portfolio/27519_DSC_0332.JPG',
+            'thumb': './static/img/gallery-work/9.jpg'
+        },
+        {
+            'src': './static/media/portfolio/27519_DSC_0332.JPG',
+            'thumb': './static/img/gallery-work/9.jpg'
+        }
+    ];
+    // gallery end
 
 }());
